5b3c3502899cbc6f3204536fb42a8954b3ae7cf8
ioedeveloper <ioedeveloper@DESKTOP-113GA6A>
2018-06-19 17:36:22 +0100

    Configuration

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname+'/dist'));
+app.use(express.static(__dirname+'/dist/officespace'));
 app.listen(process.env.PORT || 8080);
 
 app.get('/*', function(req, res){
