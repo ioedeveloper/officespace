6937815a31c34d13dcde9c73dc3ec9b2126612a2
ioedeveloper <ioedeveloper@DESKTOP-113GA6A>
2018-06-20 17:53:22 +0100

    Login

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css',
+              '../../assets/css/bootstrap.min.css',
+              '../../assets/css/font-awesome.min.css',
+              '../../assets/css/style.css',
+              'https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700'
+            ]
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
